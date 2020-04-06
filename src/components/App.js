1ff6abaa3c6ef24431243fdfaa6901bbbeea2d26
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-06 22:54:40 +0530

    handling requests with await

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,19 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component{
-    onSearchSubmit(term){
+   async onSearchSubmit(term){
         // console.log(term);
-        axios.get('https://api.unsplash.com/search/photos',{
+      const response= await  axios.get('https://api.unsplash.com/search/photos',{
             params:{ query: term},
             headers: {
                 Authorization:
                ' Client-ID xHbw6-oFjZsB0AdzVLQ2jflnRZ3T7N3FnKmIQlyX518'
             }
-        })
-        .then(response=>{
-            console.log(response.data.results);
         });
+        // .then(response=>{
+        //     console.log(response.data.results);
+        // });
+        console.log(response.data.results);
 
     }
 
