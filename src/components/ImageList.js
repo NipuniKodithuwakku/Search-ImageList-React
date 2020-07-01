dadd49c12fff77b470bdfff32d082f6e7e18df48
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-07-01 10:51:51 +0530

    modified app.js and ImageList.js

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const ImageList = (props)=>{
-    console.log(props.images);
+    // console.log(props.images);
+    const images=props.images.map((image)=>{
+        return <img src={image.url.regular} alt="searching result"/>
+    });
     return (
-        <div>ImageList</div>
+        <div>{images}</div>
     );
 }
 
