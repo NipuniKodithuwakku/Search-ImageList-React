c9c457de582c6d9893f4acc0ef250da7d085f823
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-07-06 10:54:36 +0530

    modified code

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,16 @@
-import React from 'react';
-import unsplash from '../api/unsplash';
-import SearchBar from './SearchBar';
-import ImageList from './ImageList';
+import React from "react";
+import SearchBar from "./SearchBar";
 
-
-class App extends React.Component{
-    state={ images:[]};
-
-   onSearchSubmit = async (term)=>{
-        // console.log(term);
-      const response= await  unsplash.get('/search/photos',{
-            params:{ query: term},
-           
-        });
-        // .then(response=>{
-        //     console.log(response.data.results);
-        // });
-
-        // console.log(response.data.results);
-        this.setState({ images:response.data.results});
-
-    }
-
-    render(){
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+  render() {
     return (
-        <div className="ui container" style={{marginTop:'10px'}}>
-            <SearchBar onSubmit={this.onSearchSubmit}/>
-            {/* Found : { this.state.images.length} Images */}
-            <ImageList images={this.state.images}/>
-        </div>
-        
-        );
-    }
+      <div className="ui container" style={{ margin: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 }
-
-
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,17 @@
-import React from 'react';
+import React from "react";
 
-class SearchBar extends React.Component{
-    // onInputChange(event){
-    //     console.log(event.target.value);
-
-    // }
-
-    // onInputClick(){
-    //     console.log("Input clicked!");
-    // }
-
-    state={term:""};
-
-    onFormSubmit(event){
-        event.preventDefault();
-        this.props.onSubmit(this.state.term);
-    }
-
-    render(){
-        return(
-            <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Image search</label>
-                         {/* <input type="text" onClick={this.onInputClick} onChange={this.onInputChange}/> */}
-                         <input type="text" value ={this.state.term} onChange={(e)=>this.setState({term:e.target.value.toUpperCase()})}/> 
-                    </div>
-                </form>
-
-            </div>
-        );
-    };
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image search</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-
 export default SearchBar;
