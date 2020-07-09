a877f78ee504a27d4e4cf900e2baa0984cda5905
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-07-09 14:40:08 +0530

    modified imagelist

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
@@ -17,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ margin: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        found:{this.state.images.length} images
+        <ImageList />
       </div>
     );
   }
